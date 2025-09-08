5f61663918f266ca7c9c3f940fe1edf54a23d8bf
naveenrai123 <146715461+naveenrai123@users.noreply.github.com>
2025-09-08 11:43:58 +0530

    Update advanceapp.py

--- advanceapp.py
+++ advanceapp.py
@@ -88,8 +88,10 @@ def summarize_youtube_video(url, llm, target_lang="auto"):
         try:
             
             proxies = {
-            "http": "http://hgvenkry:3up4wvgtzpft@23.95.150.145:6114",
-            "https": "http://hgvenkry:3up4wvgtzpft@23.95.150.145:6114"
+            "http": "http://hgvenkry:3up4wvgtzpft@216.10.27.159:6837",
+            "https":"http://hgvenkry:3up4wvgtzpft@216.10.27.159:6837",
+            "http": "http://hgvenkry:3up4wvgtzpft@45.43.186.39:6257",
+            "https": "http://hgvenkry:3up4wvgtzpft@45.43.186.39:6257"
             }
             
             transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'hi'],proxies=proxies)
@@ -249,6 +251,7 @@ with tab1:
                 summary = summarize_youtube_video(video_url_sum, llm, target_lang=lang_code)
                 st.success("✅ Summary Generated!")
                 st.write(summary)
+
 
 
 
